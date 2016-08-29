7bf3327a66e49f91c25b754f5d5a6656d9bef6ad
unknown <Kalin@KALINWS2014.cstcis.cti.depaul.edu>
2016-08-29 16:09:10 -0500

    grinding away on validation...

--- csda_h.h
+++ csda_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Mon Aug 29 13:41:43 2016
+/* at Mon Aug 29 16:08:04 2016
  */
 /* Compiler settings for csda.odl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
